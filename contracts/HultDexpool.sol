9e11e11af37daade0281ee672510e97c60ad4103
asujan205 <asujan205@gmail.com>
2023-08-04 20:45:52 +0545

    mint

--- contracts/HultDexpool.sol
+++ contracts/HultDexpool.sol
@@ -61,6 +61,25 @@ function setFee(uint256 fee) onlyOwner public {
 
 
 
+function mint(address to, uint128 _amount) external {
+
+    require(_amount > 0, "amount must be greater than 0");
+
+    uint256 _token1Amount = _amount * _reversePrice;
+
+    uint256 _fee = _token1Amount * _Fee / 10000;
+
+    uint256 _token1AmountAfterFee = _token1Amount - _fee;
+
+    IERC20(Token1).transferFrom(msg.sender, address(this), _token1AmountAfterFee);
+
+    userLiqiuidityPosition[to].liquidity = _amount;
+    userLiqiuidityPosition[to].token1Amount = _token1AmountAfterFee;
+
+    emit minted(to, _amount, _token1AmountAfterFee);
+
+}
+
 
 
 }
