98a3fcf3235d05f7563d3237975e04760569e7b3
asujan205 <asujan205@gmail.com>
2023-08-05 10:33:13 +0545

    #Liquidity

--- contracts/LiquidityProvider.sol
+++ contracts/LiquidityProvider.sol
@@ -12,7 +12,27 @@ import "./HultDexpool.sol";
 contract LiquidityManger {
 
 
+    address public immutable pool;
 
+
+    constructor(address _pool) {
+        pool = _pool;
+    }
+
+
+    function addLiquidity (uint128 _amount) public {
+        IERC20(HultDexpool(pool).Token1()).transferFrom(msg.sender, pool, _amount);
+        HultDexpool(pool).mint(msg.sender,_amount);
+    }
+
+
+
+
+
+function  removeLiquidity(uint128 _liquidity) public {
+        HultDexpool(pool).burn(_liquidity);
+        IERC20(HultDexpool(pool).Token1()).transferFrom(pool , msg.sender,_liquidity);
+    }
 
 
 
